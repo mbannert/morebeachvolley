7a62aab7536839aa61d4519eceebc8306aa20d95
Matt Bannert <bunny@hey.com>
2025-01-29 22:50:40 +0100

    cleaning up things for the release version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from '@astrojs/tailwind';
+
+
+import icon from 'astro-icon';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://morebeachvolley.ch",
+  output: 'server',
+
+  adapter: node({
+    mode: 'middleware'
+  }),
+
+  integrations: [tailwind(), sitemap(), icon()]
+});
---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+import { z, defineCollection, render } from 'astro:content';
+
+
+const heroCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      buttontext: z.string(),
+      teasertitle: z.string(),
+      teaser: z.string(),
+    })
+  });
+
+  const featureCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      buttontext: z.string(),
+      teasertitle: z.string(),
+      teaser: z.string(),
+    })
+  });
+
+
+
+  export const collections = {
+    'hero': heroCollection,
+    'feature': featureCollection
+  };
+  
